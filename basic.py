e8a74b7e4a9ccf9d4572d8de97bfc2dafeac91d9
Simon <S.M.Horrocks2@newcastle.ac.uk>
2022-05-09 17:22:59 +0100

    added docstrings

--- basic.py
+++ basic.py
@@ -5,6 +5,10 @@ from graph import Graph
 
 class Constant(Graph):
     def __init__(self, c):
+        """
+
+        :param c: the constant output of the graph
+        """
 
         Graph.__init__(self, 0, c)
 
@@ -12,6 +16,12 @@ class Constant(Graph):
         return str(self.c)
 
     def func(self, x):
+        """
+        Given any input returns the constant
+
+        :param x: input for the graph function
+        :return: the constant (self.c)
+        """
         return self.c
 
 
@@ -20,10 +30,23 @@ class Linear(Graph):
         return f"{self.m}x{signed(self.c, no_zero=True)}"
 
     def func(self, x):
+        """
+        Computes a linear graph in the form mx+c
+
+        :param x: input for the graph function
+        :return: a Number
+        """
         return self.m * x + self.c
 
 
 def signed(number, no_zero=False):
+    """
+    Formats a number to remove a sign for outputting
+
+    :param number: the number to convert to a signed string
+    :param no_zero: if True zero returns an empty string
+    :return: signed string of the number
+    """
     if number > 0:
         return f"+{number}"
     elif no_zero and number == 0:
@@ -34,6 +57,11 @@ def signed(number, no_zero=False):
 
 class Polynomial(Graph):
     def __init__(self, m, c):
+        """
+
+        :param m: list of coefficients and indexes in the form [(coefficient, index), ... ]
+        :param c: constant coefficient
+        """
 
         for i in filter(lambda x: x[1] == 0, m):
             c += i[0]
@@ -44,11 +72,23 @@ class Polynomial(Graph):
         return f"{self.m[0][0]}x^{self.m[0][1]}" + "".join(f"{signed(coefficient)}x^{index}" for coefficient, index in self.m[1:]) + signed(self.c, no_zero=True)
 
     def func(self, x):
+        """
+        Computes a polynomial graph in the form mnx^in+ ... +c (where n is the number of elements in m)
+        :param x: the input for the graph function
+        :return: a Number
+        """
         return sum(coefficient * x ** index for coefficient, index in self.m) + self.c
 
 
 class Functional(Graph):
     def __init__(self, function, graph, *args, **kwargs):
+        """
+
+        :param function: a function to wrap around the result of the graph function
+        :param graph: the graph to wrap
+        :param args: any additional args for the graph function
+        :param kwargs: any additional kwargs for the graph function
+        """
 
         Graph.__init__(self, 0, 0)
 
@@ -64,6 +104,11 @@ class Functional(Graph):
         return f"{self.name}({self.graph})"
 
     def func(self, x):
+        """
+        Computes a graph function wrapped with a mathematical function such that g(f(x))
+        :param x: the input for the graph function
+        :return: a Number
+        """
         return self.function(x)
 
 
@@ -75,5 +120,10 @@ class Absolute(Functional):
 
 class Logarithmic(Functional):
     def __init__(self, graph, base=10):
+        """
+
+        :param graph: the graph to wrap
+        :param base: the base of log to use (default is base=10)
+        """
 
         Functional.__init__(self, math.log, graph, base=base)
--- graph.py
+++ graph.py
@@ -3,10 +3,25 @@ import math
 
 class Graph:
     def __init__(self, m, c):
+        """
+
+        :param m: gradient of the graph
+        :param c: constant coefficient
+        """
         self.m = m
         self.c = c
 
     def __call__(self, x=None, lb=None, ub=None, *args, **kwargs):
+        """
+        Run the graph function or create a period
+
+        :param x: the input for the graph
+        :param lb: lower bound
+        :param ub: upper bound
+        :param args: any additional args for the graph function
+        :param kwargs: any additional kwargs for the graph function
+        :return: a Period or Number
+        """
         if lb or ub:
             return Period(self, lb, ub)
         elif x is not None:
@@ -16,11 +31,23 @@ class Graph:
         return f"Graph(m={self.m}, c={self.c})"
 
     def func(self, x):
+        """
+        The function for the graph
+
+        :param x: the input
+        :return: a Number (y-value)
+        """
         return 0
 
 
 class Period:
     def __init__(self, graph, lb, ub):
+        """
+
+        :param graph: the graph over which the period operates
+        :param lb: the lower bound of the domain (defaults to -inf if None)
+        :param ub: the upper bound of the domain (defaults to inf if None)
+        """
         self.graph = graph
 
         if not lb: lb = -math.inf
@@ -33,9 +60,23 @@ class Period:
         self.ub = ub
 
     def __call__(self, x, *args, **kwargs):
+        """
+        Runs the graph function
+
+        :param x: the input for the graph
+        :param args: any additional args for the graph function
+        :param kwargs: any additional kwargs for the graph function
+        :return: a Period or Number
+        """
         return self.graph.func(x)
 
     def __rshift__(self, other):
+        """
+        Creates a periodic to compose the periods
+
+        :param other: the period to follow
+        :return: a Periodic
+        """
         if isinstance(other, Period) and self.ub and other.lb:
             if self.ub == other.lb:
                 return Periodic(self, other)
@@ -48,14 +89,31 @@ class Period:
 
 class Periodic:
     def __init__(self, *periods):
+        """
+
+        :param periods: the periods in this Periodic (assumed to be in order)
+        """
         self.periods = list(periods)
 
     def __call__(self, x, *args, **kwargs):
+        """
+        Run the graph function for the matching period
+
+        :param x: the input for the graph
+        :param args: any additional args for the graph function
+        :param kwargs: any additional kwargs for the graph function
+        :return: a Number
+        """
         for p in self.periods:
             if p.lb < x <= p.ub:
                 return p(x)
 
     def __rshift__(self, other):
+        """
+        Add Period on the right to the Periodic
+        :param other: the period to append
+        :return: a Periodic (self)
+        """
         if isinstance(other, Period) and self.periods[-1].ub and other.lb:
             if self.periods[-1].ub == other.lb:
                 self.periods.append(other)
